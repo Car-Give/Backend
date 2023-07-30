c283fbb4dbe11d0be32807f18b2543c828e0c3aa
GaBaljaintheroom <junsu1222@naver.com>
2023-07-30 18:43:46 +0900

    chore: 스프링 부트 초기 세팅

---
+++ src/main/java/com/example/cargive/CarGiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.cargive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarGiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cargive/CarGiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cargive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
