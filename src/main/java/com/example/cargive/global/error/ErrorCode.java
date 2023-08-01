afbf898558b31459239b4aabb868c59785d9047a
GaBaljaintheroom <junsu1222@naver.com>
2023-08-01 16:45:57 +0900

    feat : FavoriteNotFoundException 에러코드 작성

--- src/main/java/com/example/cargive/global/error/ErrorCode.java
+++ src/main/java/com/example/cargive/global/error/ErrorCode.java
@@ -9,7 +9,10 @@ public enum ErrorCode {
 
     //server
     INTERNAL_SERVER_ERROR(500, "G001", "서버 오류"),
-    INPUT_INVALID_VALUE(400, "G002", "잘못된 입력");
+    INPUT_INVALID_VALUE(400, "G002", "잘못된 입력"),
+
+    //Favorite
+    FAVORITE_NOT_FOUND_ERROR(400, "F001", "존재하지 않는 즐겨찾기 입니다.");
 
 
     private final int status;
