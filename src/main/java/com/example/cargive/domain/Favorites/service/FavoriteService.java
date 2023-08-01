f0b47668bd8c5004505db7d4a169140eba0f3495
GaBaljaintheroom <junsu1222@naver.com>
2023-08-01 16:45:16 +0900

    feat : Favorite 삭제 Service 작성

--- src/main/java/com/example/cargive/domain/Favorites/service/FavoriteService.java
+++ src/main/java/com/example/cargive/domain/Favorites/service/FavoriteService.java
@@ -4,9 +4,11 @@ package com.example.cargive.domain.Favorites.service;
 import com.example.cargive.domain.Favorites.dto.mapper.FavoriteMapper;
 import com.example.cargive.domain.Favorites.dto.request.FavoriteCreateRequest;
 import com.example.cargive.domain.Favorites.entity.Favorite;
+import com.example.cargive.domain.Favorites.exception.FavoriteNotFoundException;
 import com.example.cargive.domain.Favorites.repository.FavoritesRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +20,15 @@ public class FavoriteService {
     public void createFavorite(FavoriteCreateRequest request){
        Favorite newFavorite = favoriteMapper.toCreateRequest(request);
        favoritesRepository.save(newFavorite);
+    }
+
+    public void deleteFavorite(Long favoriteId){
+        Favorite deleteFavorite = findFavoriteById(favoriteId);
+        favoritesRepository.delete(deleteFavorite);
+    }
+
+    @Transactional(readOnly = true)
+    public Favorite findFavoriteById(Long favoriteId){
+        return favoritesRepository.findById(favoriteId).orElseThrow(FavoriteNotFoundException::new);
     }
 }
