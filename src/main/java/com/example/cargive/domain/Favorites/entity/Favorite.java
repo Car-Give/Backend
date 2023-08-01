47a999ff6f5fbc95ef1b54bea69db16db5a17c25
GaBaljaintheroom <junsu1222@naver.com>
2023-08-01 15:26:18 +0900

    feat : 즐겨찾기 엔티티 작성

---
+++ src/main/java/com/example/cargive/domain/Favorites/entity/Favorite.java
@@ -0,0 +1,28 @@
+package com.example.cargive.domain.Favorites.entity;
+
+import com.example.cargive.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Favorites")
+public class Favorite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "favorite_name", length = 30)
+    private String name;
+
+    private Long count;
+
+    @Builder
+    public Favorite(String name, Long count){
+        this.name = name;
+        this.count = count;
+    }
+
+}
