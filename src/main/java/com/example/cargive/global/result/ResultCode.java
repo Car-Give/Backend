5296cb2e06558c20585fae45c35e2328357b309c
GaBaljaintheroom <junsu1222@naver.com>
2023-08-03 01:50:01 +0900

    feat : ParkingLot 생성 응답 코드 작성

--- src/main/java/com/example/cargive/global/result/ResultCode.java
+++ src/main/java/com/example/cargive/global/result/ResultCode.java
@@ -9,7 +9,10 @@ public enum ResultCode {
 
     //즐겨찾기
     FAVORITE_CREATE_SUCCESS("F001", "즐겨찾기 생성 성공"),
-    FAVORITE_DELETE_SUCCESS("F002", "즐겨찾기 삭제 성공");
+    FAVORITE_DELETE_SUCCESS("F002", "즐겨찾기 삭제 성공"),
+
+    // 주차장
+    PARKING_LOT_CREATE_SUCCESS("P001", "주차장 생성 성공");
 
     private final String code;
     private final String message;
