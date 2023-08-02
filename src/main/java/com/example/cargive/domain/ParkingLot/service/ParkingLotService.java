6f3b977cbbb326dd19d4713eb1a90b596a6e5999
GaBaljaintheroom <junsu1222@naver.com>
2023-08-03 02:29:54 +0900

    feat : ParkingLot 삭제 Service 작성

--- src/main/java/com/example/cargive/domain/ParkingLot/service/ParkingLotService.java
+++ src/main/java/com/example/cargive/domain/ParkingLot/service/ParkingLotService.java
@@ -4,9 +4,13 @@ import com.example.cargive.domain.Favorites.entity.Favorite;
 import com.example.cargive.domain.Favorites.service.FavoriteService;
 import com.example.cargive.domain.ParkingLot.dto.mapper.ParkingLotMapper;
 import com.example.cargive.domain.ParkingLot.dto.request.ParkingLotCreateRequest;
+import com.example.cargive.domain.ParkingLot.entity.ParkingLot;
+import com.example.cargive.domain.ParkingLot.exception.ParkingLotNotFoundException;
 import com.example.cargive.domain.ParkingLot.repository.ParkingLotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 @RequiredArgsConstructor
 public class ParkingLotService {
@@ -20,6 +24,17 @@ public class ParkingLotService {
         Favorite findFavorite = favoriteService.findFavoriteById(request.getFavoriteId());
         findFavorite.upCount();
         parkingLotRepository.save(parkingLotMapper.toCreateRequest(request, findFavorite));
+    }
+
+    public void deleteParkingLot(Long parkingLotId){
+        ParkingLot parkingLot = findParkingLotById(parkingLotId);
+        parkingLot.updateFavoriteCount();
+        parkingLotRepository.delete(parkingLot);
+    }
+
+    @Transactional(readOnly = true)
+    public ParkingLot findParkingLotById(Long parkingLotId){
+        return parkingLotRepository.findById(parkingLotId).orElseThrow(ParkingLotNotFoundException::new);
     }
 
 }
