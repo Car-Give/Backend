fa1e2ff91fbf98ffb1b18097519f8df6815dcdaa
GaBaljaintheroom <junsu1222@naver.com>
2023-08-03 15:47:57 +0900

    feat : 주차장 조회 Mapper 작성

--- src/main/java/com/example/cargive/domain/ParkingLot/dto/mapper/ParkingLotMapper.java
+++ src/main/java/com/example/cargive/domain/ParkingLot/dto/mapper/ParkingLotMapper.java
@@ -2,8 +2,14 @@ package com.example.cargive.domain.ParkingLot.dto.mapper;
 
 import com.example.cargive.domain.Favorites.entity.Favorite;
 import com.example.cargive.domain.ParkingLot.dto.request.ParkingLotCreateRequest;
+import com.example.cargive.domain.ParkingLot.dto.response.ParkingLotInfo;
+import com.example.cargive.domain.ParkingLot.dto.response.ParkingLotSliceInfo;
 import com.example.cargive.domain.ParkingLot.entity.ParkingLot;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ParkingLotMapper {
@@ -16,6 +22,21 @@ public class ParkingLotMapper {
                 .fee(request.getFee())
                 .favorite(favorite)
                 .build();
+    }
+
+    public ParkingLotInfo toEntity(ParkingLot parkingLot){
+        return ParkingLotInfo.builder()
+                .parkingLotId(parkingLot.getId())
+                .name(parkingLot.getName())
+                .phoneNumber(parkingLot.getPhoneNumber())
+                .address(parkingLot.getAddress())
+                .fee(parkingLot.getFee())
+                .build();
+    }
+
+    public ParkingLotSliceInfo toParkingLotInfoList(Slice<ParkingLot> parkingLots){
+        List<ParkingLotInfo> parkingLotInfos = parkingLots.stream().map(this::toEntity).toList();
+        return ParkingLotSliceInfo.builder().parkingLotInfoList(parkingLotInfos).build();
     }
 
 }
