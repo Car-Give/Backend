e491e8a7526f9bfcb068738921ff3849f9c9c9e2
GaBaljaintheroom <junsu1222@naver.com>
2023-08-03 14:57:56 +0900

    feat : 즐겨찾기 엔티티 @OneToMany 추가

--- src/main/java/com/example/cargive/domain/Favorites/entity/Favorite.java
+++ src/main/java/com/example/cargive/domain/Favorites/entity/Favorite.java
@@ -1,8 +1,12 @@
 package com.example.cargive.domain.Favorites.entity;
 
+import com.example.cargive.domain.ParkingLot.entity.ParkingLot;
 import com.example.cargive.global.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,10 +25,16 @@ public class Favorite extends BaseEntity {
     @Column(nullable = false)
     private Long count;
 
+    @OneToMany(mappedBy = "favorite",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<ParkingLot> parkingLots;
+
     @Builder
     public Favorite(String name, Long count){
         this.name = name;
         this.count = count;
+        this.parkingLots = new ArrayList<>();
     }
 
     public void upCount(){
