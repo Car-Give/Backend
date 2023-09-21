5212d35aee0291996119b9dccc9f0b3f6b2921b0
길지운 <wldns2577@khu.ac.kr>
2023-09-22 03:00:08 +0900

    fix: BaseResponse 템플릿 수정에 따른 기존 API 응답 형식 수정

--- src/main/java/com/example/cargive/domain/parkingLot/controller/ParkingLotController.java
+++ src/main/java/com/example/cargive/domain/parkingLot/controller/ParkingLotController.java
@@ -7,6 +7,7 @@ import com.example.cargive.global.base.BaseResponse;
 import com.example.cargive.global.base.BaseResponseStatus;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,20 +17,20 @@ public class ParkingLotController {
     private final ParkingLotService parkingLotService;
 
     @GetMapping("/{parkingLotId}") // 주차장 단일 조회
-    public BaseResponse<ParkingLotResponse> findParkingLot(@PathVariable Long parkingLotId) {
+    public ResponseEntity<BaseResponse> findParkingLot(@PathVariable Long parkingLotId) {
         ParkingLotResponse findPkLot = parkingLotService.findParkingLot(parkingLotId);
-        return new BaseResponse<>(BaseResponseStatus.SUCCESS, findPkLot);
+        return BaseResponse.toResponseEntityContainsResult(findPkLot);
     }
 
     @PostMapping // 주차장 추가
-    public BaseResponse<BaseResponseStatus> createParkingLot(@RequestBody @Valid ParkingLotRequest request) {
+    public ResponseEntity<BaseResponse> createParkingLot(@RequestBody @Valid ParkingLotRequest request) {
         parkingLotService.createParkingLot(request);
-        return new BaseResponse<>(BaseResponseStatus.CREATED);
+        return BaseResponse.toResponseEntityContainsStatus(BaseResponseStatus.CREATED);
     }
 
     @DeleteMapping("/{parkingLotId}") // 주차장 삭제
-    public BaseResponse<BaseResponseStatus> deleteParkingLot(@PathVariable Long parkingLotId) {
+    public ResponseEntity<BaseResponse> deleteParkingLot(@PathVariable Long parkingLotId) {
         parkingLotService.deleteParkingLot(parkingLotId);
-        return new BaseResponse<>(BaseResponseStatus.SUCCESS);
+        return BaseResponse.toResponseEntityContainsStatus(BaseResponseStatus.SUCCESS);
     }
 }
