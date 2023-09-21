31dd63f38be6fd20785d1b5056bf382c229d6c28
길지운 <wldns2577@khu.ac.kr>
2023-09-22 03:30:58 +0900

    test: Question Paging API Controller 테스트 코드 추가

--- src/test/java/com/example/cargive/question/controller/QuestionFindControllerTest.java
+++ src/test/java/com/example/cargive/question/controller/QuestionFindControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.cargive.question.controller;
 
 import com.example.cargive.common.ControllerTest;
+import com.example.cargive.domain.question.service.response.GetListOfQuestionResponse;
 import com.example.cargive.domain.question.service.response.LoadQuestionResponse;
 import com.example.cargive.global.base.BaseException;
 import com.example.cargive.global.base.BaseResponseStatus;
@@ -11,7 +12,10 @@ import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static com.example.cargive.question.fixture.QuestionFixture.QUESTION_0;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.cargive.question.fixture.QuestionFixture.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
@@ -173,11 +177,129 @@ public class QuestionFindControllerTest extends ControllerTest {
         }
     }
 
+    @Nested
+    @DisplayName("질문 리스트 조회 API [GET /api/question/page]")
+    class GetListOfQuestion {
+        private static final String BASE_URL = "/api/question/page";
+        private static final Long MEMBER_ID = 1L;
+        private static final Long LAST_QUESTION_ID = -1L;
+
+        @Test
+        @DisplayName("유효하지 않은 질문 ID를 입력받으면 질문 리스트 조회에 실패한다")
+        void throwExceptionByInvalidQuestionId() throws Exception {
+            // given
+            doThrow(new BaseException(BaseResponseStatus.QUESTION_NOT_FOUND_ERROR))
+                    .when(questionFindService)
+                    .getListOfQuestion(anyLong(), anyLong());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .get(BASE_URL)
+                    .param("memberId", String.valueOf(MEMBER_ID))
+                    .param("lastQuestionId", String.valueOf(ERROR_QUESTION_ID))
+                    ;
+
+            // then
+            final BaseResponseStatus expectedError = BaseResponseStatus.QUESTION_NOT_FOUND_ERROR;
+            mockMvc.perform(requestBuilder)
+                    .andExpectAll(
+                            status().isNotFound(),
+                            jsonPath("$.status").exists(),
+                            jsonPath("$.status").value(expectedError.getStatus().value()),
+                            jsonPath("$.code").exists(),
+                            jsonPath("$.code").value(expectedError.getCode()),
+                            jsonPath("$.message").exists(),
+                            jsonPath("$.message").value(expectedError.getMessage())
+                    )
+                    .andDo(
+                            document(
+                                    "QuestionFind/GetList/Failure/Case1",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    queryParameters(
+                                            parameterWithName("memberId").description("사용자 ID"),
+                                            parameterWithName("lastQuestionId").description("질문 ID")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("status").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+                                            fieldWithPath("code").type(JsonFieldType.STRING).description("커스텀 상태 코드"),
+                                            fieldWithPath("message").type(JsonFieldType.STRING).description("상태 메시지")
+                                    )
+                            )
+                    );
+        }
+
+        @Test
+        @DisplayName("질문 리스트 조회에 성공한다")
+        void success() throws Exception {
+            // given
+            doReturn(getListOfQuestionResponse())
+                    .when(questionFindService)
+                    .getListOfQuestion(anyLong(), anyLong());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .get(BASE_URL)
+                    .param("memberId", String.valueOf(MEMBER_ID))
+                    .param("lastQuestionId", String.valueOf(LAST_QUESTION_ID))
+                    ;
+
+            // then
+            mockMvc.perform(requestBuilder)
+                    .andExpectAll(
+                            status().isOk(),
+                            jsonPath("$.result[0]").exists(),
+                            jsonPath("$.result[0].id").value(QUESTION_5.getId()),
+                            jsonPath("$.result[1]").exists(),
+                            jsonPath("$.result[1].id").value(QUESTION_4.getId()),
+                            jsonPath("$.result[2]").exists(),
+                            jsonPath("$.result[2].id").value(QUESTION_3.getId()),
+                            jsonPath("$.result[3]").exists(),
+                            jsonPath("$.result[3].id").value(QUESTION_2.getId()),
+                            jsonPath("$.result[4]").exists(),
+                            jsonPath("$.result[4].id").value(QUESTION_1.getId()),
+                            jsonPath("$.result[5]").exists(),
+                            jsonPath("$.result[5].id").value(QUESTION_0.getId()),
+                            jsonPath("$.result[6]").doesNotExist()
+                    )
+                    .andDo(
+                            document(
+                                    "QuestionFind/GetList/Success",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    queryParameters(
+                                            parameterWithName("memberId").description("사용자 ID"),
+                                            parameterWithName("lastQuestionId").description("질문 ID")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("status").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+                                            fieldWithPath("code").type(JsonFieldType.STRING).description("커스텀 상태 코드"),
+                                            fieldWithPath("message").type(JsonFieldType.STRING).description("상태 메시지"),
+                                            fieldWithPath("result[].id").type(JsonFieldType.NUMBER).description("질문 ID"),
+                                            fieldWithPath("result[].title").type(JsonFieldType.STRING).description("질문 제목"),
+                                            fieldWithPath("result[].createAt").type(JsonFieldType.STRING).description("질문 작성시간")
+                                    )
+                            )
+                    );
+        }
+    }
+
     private LoadQuestionResponse getLoadQuestionResponse() {
         return LoadQuestionResponse.builder()
                 .title(QUESTION_0.getTitle())
                 .content(QUESTION_0.getContent())
                 .categoryValue(QUESTION_0.getCategory().getValue())
                 .build();
+    }
+
+    private List<GetListOfQuestionResponse> getListOfQuestionResponse() {
+        List<GetListOfQuestionResponse> responseList = new ArrayList<>();
+        responseList.add(new GetListOfQuestionResponse(QUESTION_5.getId(), QUESTION_5.getTitle(), QUESTION_5.getCreateAt()));
+        responseList.add(new GetListOfQuestionResponse(QUESTION_4.getId(), QUESTION_4.getTitle(), QUESTION_4.getCreateAt()));
+        responseList.add(new GetListOfQuestionResponse(QUESTION_3.getId(), QUESTION_3.getTitle(), QUESTION_3.getCreateAt()));
+        responseList.add(new GetListOfQuestionResponse(QUESTION_2.getId(), QUESTION_2.getTitle(), QUESTION_2.getCreateAt()));
+        responseList.add(new GetListOfQuestionResponse(QUESTION_1.getId(), QUESTION_1.getTitle(), QUESTION_1.getCreateAt()));
+        responseList.add(new GetListOfQuestionResponse(QUESTION_0.getId(), QUESTION_0.getTitle(), QUESTION_0.getCreateAt()));
+        return responseList;
     }
 }
