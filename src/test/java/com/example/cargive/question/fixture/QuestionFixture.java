8a9b388cb22737db73177a25479d2d799237f591
길지운 <wldns2577@khu.ac.kr>
2023-09-22 04:02:41 +0900

    test: Question Paging Query 테스트를 위한 Fixture 추가 및 수정

--- src/test/java/com/example/cargive/question/fixture/QuestionFixture.java
+++ src/test/java/com/example/cargive/question/fixture/QuestionFixture.java
@@ -1,6 +1,8 @@
 package com.example.cargive.question.fixture;
 
+import com.example.cargive.domain.member.entity.Member;
 import com.example.cargive.domain.question.entity.Category;
+import com.example.cargive.domain.question.entity.Question;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,12 +11,13 @@ import java.time.LocalDateTime;
 @Getter
 @RequiredArgsConstructor
 public enum QuestionFixture {
-    QUESTION_0(0L, "질문0", "질문내용0", Category.COMMON, LocalDateTime.now()),
-    QUESTION_1(1L, "질문1", "질문내용1", Category.COMMON, LocalDateTime.now().plusMinutes(1)),
-    QUESTION_2(2L, "질문2", "질문내용2", Category.BUG, LocalDateTime.now().plusMinutes(2)),
-    QUESTION_3(3L, "질문3", "질문내용3", Category.BUG, LocalDateTime.now().plusMinutes(3)),
-    QUESTION_4(4L, "질문4", "질문내용4", Category.SUGGEST, LocalDateTime.now().plusMinutes(4)),
-    QUESTION_5(5L, "질문5", "질문내용5", Category.ETC, LocalDateTime.now().plusMinutes(5)),
+    QUESTION_0(1L, "질문0", "질문내용0", Category.COMMON, LocalDateTime.now()),
+    QUESTION_1(2L, "질문1", "질문내용1", Category.COMMON, LocalDateTime.now().plusMinutes(1)),
+    QUESTION_2(3L, "질문2", "질문내용2", Category.BUG, LocalDateTime.now().plusMinutes(2)),
+    QUESTION_3(4L, "질문3", "질문내용3", Category.BUG, LocalDateTime.now().plusMinutes(3)),
+    QUESTION_4(5L, "질문4", "질문내용4", Category.SUGGEST, LocalDateTime.now().plusMinutes(4)),
+    QUESTION_5(6L, "질문5", "질문내용5", Category.ETC, LocalDateTime.now().plusMinutes(5)),
+    QUESTION_6(7L, "질문6", "질문내용6", Category.ETC, LocalDateTime.now().plusMinutes(6)),
     ;
 
     private final Long id;
@@ -22,4 +25,13 @@ public enum QuestionFixture {
     private final String content;
     private final Category category;
     private final LocalDateTime createAt;
+
+    public Question toQuestion(Member member) {
+        return Question.builder()
+                .title(title)
+                .content(content)
+                .category(category)
+                .member(member)
+                .build();
+    }
 }
