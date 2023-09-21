9208d428a192622affb3648cea426bf17c9a34bb
길지운 <wldns2577@khu.ac.kr>
2023-09-22 03:05:34 +0900

    feat: Question Paging Service 구현

--- src/main/java/com/example/cargive/domain/question/service/QuestionFindService.java
+++ src/main/java/com/example/cargive/domain/question/service/QuestionFindService.java
@@ -1,12 +1,15 @@
 package com.example.cargive.domain.question.service;
 
 import com.example.cargive.domain.member.entity.Member;
-import com.example.cargive.domain.question.controller.dto.response.LoadQuestionResponse;
 import com.example.cargive.domain.question.entity.Question;
 import com.example.cargive.domain.question.entity.repository.QuestionRepository;
+import com.example.cargive.domain.question.service.response.GetListOfQuestionResponse;
+import com.example.cargive.domain.question.service.response.LoadQuestionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -25,5 +28,17 @@ public class QuestionFindService {
                 .content(question.getContent())
                 .categoryValue(question.getCategory().getValue())
                 .build();
+    }
+
+    public List<GetListOfQuestionResponse> getListOfQuestion(Long memberId, Long questionId) {
+        Member member = questionService.findMember(memberId);
+        Question question = configPaging(questionId);
+        return questionRepository.findPageOrderByCreateAt(member, question.getCreateAt());
+    }
+
+    private Question configPaging(Long questionId) {
+        if (questionId == -1)
+            return Question.builder().build();
+        return questionService.findQuestion(questionId);
     }
 }
