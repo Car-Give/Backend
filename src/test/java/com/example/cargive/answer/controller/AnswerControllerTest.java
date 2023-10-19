2dfc91f8f9102df74afe520481aca7972f00c119
minsub <chrkb1569@naver.com>
2023-10-19 11:53:09 +0900

    doc : Answer Entity API 명세서 작성

--- src/test/java/com/example/cargive/answer/controller/AnswerControllerTest.java
+++ src/test/java/com/example/cargive/answer/controller/AnswerControllerTest.java
@@ -16,8 +16,9 @@ import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -62,6 +63,12 @@ public class AnswerControllerTest extends ControllerTest {
                                     "Answer/Create/Failure/Case1",
                                     preprocessRequest(prettyPrint()),
                                     preprocessResponse(prettyPrint()),
+                                    pathParameters(
+                                            parameterWithName("questionId").description("질문 Id")
+                                    ),
+                                    requestFields(
+                                            fieldWithPath("content").type(JsonFieldType.STRING).description("답변 내용")
+                                    ),
                                     responseFields(
                                             fieldWithPath("status").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
                                             fieldWithPath("code").type(JsonFieldType.STRING).description("커스텀 상태 코드"),
@@ -93,7 +100,13 @@ public class AnswerControllerTest extends ControllerTest {
                             document(
                                     "Answer/Create/Success",
                                     preprocessRequest(prettyPrint()),
-                                    preprocessResponse(prettyPrint())
+                                    preprocessResponse(prettyPrint()),
+                                    pathParameters(
+                                            parameterWithName("questionId").description("질문 Id")
+                                    ),
+                                    requestFields(
+                                            fieldWithPath("content").type(JsonFieldType.STRING).description("답변 내용")
+                                    )
                     )
                     );
         }
@@ -138,6 +151,12 @@ public class AnswerControllerTest extends ControllerTest {
                                     "Answer/Edit/Failure/Case1",
                                     preprocessRequest(prettyPrint()),
                                     preprocessResponse(prettyPrint()),
+                                    pathParameters(
+                                      parameterWithName("answerId").description("질문 Id")
+                                    ),
+                                    requestFields(
+                                            fieldWithPath("content").type(JsonFieldType.STRING).description("수정할 답변 내용")
+                                    ),
                                     responseFields(
                                             fieldWithPath("status").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
                                             fieldWithPath("code").type(JsonFieldType.STRING).description("커스텀 상태 코드"),
@@ -169,7 +188,18 @@ public class AnswerControllerTest extends ControllerTest {
                             document(
                                     "Answer/Edit/Success",
                                     preprocessRequest(prettyPrint()),
-                                    preprocessResponse(prettyPrint()))
+                                    preprocessResponse(prettyPrint()),
+                                    pathParameters(
+                                    parameterWithName("answerId").description("질문 Id")
+                                    ),
+                                    requestFields(
+                                            fieldWithPath("content").type(JsonFieldType.STRING).description("수정할 답변 내용")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("status").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+                                            fieldWithPath("code").type(JsonFieldType.STRING).description("커스텀 상태 코드"),
+                                            fieldWithPath("message").type(JsonFieldType.STRING).description("상태 메시지"))
+                            )
                     );
         }
     }
@@ -210,6 +240,9 @@ public class AnswerControllerTest extends ControllerTest {
                                     "Answer/Delete/Failure/Case1",
                                     preprocessRequest(prettyPrint()),
                                     preprocessResponse(prettyPrint()),
+                                    pathParameters(
+                                            parameterWithName("answerId").description("질문 Id")
+                                    ),
                                     responseFields(
                                             fieldWithPath("status").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
                                             fieldWithPath("code").type(JsonFieldType.STRING).description("커스텀 상태 코드"),
@@ -238,7 +271,14 @@ public class AnswerControllerTest extends ControllerTest {
                             document(
                                     "Answer/Delete/Success",
                                     preprocessRequest(prettyPrint()),
-                                    preprocessResponse(prettyPrint())
+                                    preprocessResponse(prettyPrint()),
+                                    pathParameters(
+                                            parameterWithName("answerId").description("질문 Id")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("status").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+                                            fieldWithPath("code").type(JsonFieldType.STRING).description("커스텀 상태 코드"),
+                                            fieldWithPath("message").type(JsonFieldType.STRING).description("상태 메시지"))
                             )
                     );
         }
